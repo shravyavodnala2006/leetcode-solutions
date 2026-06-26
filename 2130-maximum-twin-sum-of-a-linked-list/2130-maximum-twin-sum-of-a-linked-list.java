f28ce328fb8b4080575e5c8900077d6299f7849c
shravyavodnala2006 <shravyavodnala2006@gmail.com>
2026-06-26 22:41:23 +0530

    Time: 7 ms (39.93%), Space: 100.9 MB (94.27%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -33,12 +33,15 @@ class Solution {
         return s;
     }
     static ListNode reverseList(ListNode head) {
-        if(head==null || head.next==null){
-            return head;
-        }
-        ListNode rh=reverseList(head.next);
-        head.next.next=head;
-        head.next=null;
-        return rh;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+    }
+
+    return prev;
     }
 }
