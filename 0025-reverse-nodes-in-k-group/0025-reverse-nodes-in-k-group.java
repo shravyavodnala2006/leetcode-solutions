17f2dcadecdb12837ca2cba898a03f897872451b
shravyavodnala2006 <shravyavodnala2006@gmail.com>
2026-06-26 20:35:59 +0530

    Time: 0 ms (100%), Space: 46.7 MB (16.32%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -58,13 +58,6 @@ class Solution {
         }
         return null;
     }
-    static int len(ListNode temp){
-        int cnt=0;
-        while(temp!=null){
-            cnt++;
-            temp=temp.next;
-        }
-        return cnt;
-    }
+   
 
 }
