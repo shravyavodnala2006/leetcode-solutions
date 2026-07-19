9e430967df4a5a4edb5aca20ed6af3c6393d50c7
shravyavodnala2006 <shravyavodnala2006@gmail.com>
2026-07-19 14:43:39 +0530

    Time: 4 ms (19.25%), Space: 43.3 MB (32.1%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -2,16 +2,25 @@ class Solution {
     public int countGoodSubstrings(String s) {
         int n=s.length();
         int k=3;
-        int cnt=0;       
-        for(int i=0;i<=n-k;i++){
-            boolean dist=true;
-            HashSet<Character>st=new HashSet<>();
-            for(int j=i;j<=i+k-1;j++){
-                char ch=s.charAt(j);
-                if(!st.contains(ch)) st.add(ch);
-                else dist=false; 
+        int cnt=0;
+        int l=0;
+        int r=0;      
+        HashMap<Character,Integer>map=new HashMap<>();
+        while(r<n){
+            char ch=s.charAt(r); 
+            map.put(ch,map.getOrDefault(ch,0)+1);              
+            if(r-l==k){
+                char left=s.charAt(l);
+                map.put(left,map.getOrDefault(left,0)-1);
+                if (map.get(left) == 0) 
+                    map.remove(left);
+                l++;
+                             
             }
-            if(dist) cnt++;
+            if(r-l+1==k){
+                if(map.size()==k) cnt++;
+            }
+            r++;
         }
         return cnt;
     }
